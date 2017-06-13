bcb1e54ba50701732dfcd2442bdcc65bc7ac3287
川上 洋介 <y-kawakami@deltawing.co.jp>
2017-06-13 17:30:15 +0900

    first commit

---
+++ SampleFragment/app/src/main/java/com/github/kawakami/yosuke/simple_sample_codes/samplefragment/Fragment_a.java
@@ -0,0 +1,21 @@
+package com.github.kawakami.yosuke.simple_sample_codes.samplefragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by y-kawakami on 2017/06/13.
+ */
+
+public class Fragment_a extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a, null);
+    }
+}
---
+++ SampleFragment/app/src/main/java/com/github/kawakami/yosuke/simple_sample_codes/samplefragment/MainActivity.java
@@ -0,0 +1,50 @@
+package com.github.kawakami.yosuke.simple_sample_codes.samplefragment;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    List<String> mTags = Arrays.asList("a", "b", "c");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null)
+        {
+            getFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, new Fragment_a(), "a")
+                .commit();
+        }
+    }
+
+
+    public void changeFragment(View view)
+    {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+        String   tag   = getFragmentManager().findFragmentById(R.id.container).getTag();
+        int      index = mTags.indexOf(tag);
+
+        switch(index)
+        {
+            case  2: ft.add(R.id.container, new Fragment_a(), "a"); break;
+            case  1: ft.add(R.id.container, new Fragment_c(), "c"); break;
+            case  0:
+            default: ft.add(R.id.container, new Fragment_b(), "b"); break;
+        }
+
+        ft.commit();
+    }
+}
