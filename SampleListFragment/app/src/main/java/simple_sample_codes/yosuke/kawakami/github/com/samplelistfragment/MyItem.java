a0f999350361f2228658fe20f7d2d9640bcf21c9
unknown <y-kawakami@central.deltawing.co.jp>
2017-06-23 18:16:23 +0900

    Modify : 削除忘れのコメントアウトを削除

--- SampleListFragment/app/src/main/java/simple_sample_codes/yosuke/kawakami/github/com/samplelistfragment/MyItem.java
+++ SampleListFragment/app/src/main/java/simple_sample_codes/yosuke/kawakami/github/com/samplelistfragment/MyItem.java
@@ -4,7 +4,6 @@ package simple_sample_codes.yosuke.kawakami.github.com.samplelistfragment;
  * Created by y-kawakami on 2017/06/20.
  */
 
-// public class MyItem implements Parcelable
 public class MyItem
 {
     String title;
